26c6bac47723bc810263614d3cb51bca392666f8
Youssef Shoaib <yhshoaib@students.wcpss.net>
2019-10-24 07:41:17 -0400

    Finish vehicle class

--- BattleBots/Vehicle.cs
+++ BattleBots/Vehicle.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace BattleBots
 {
-    public abstract class Vehicle
+    public abstract class Vehicle : Player
     {
+        public const string WEAPON_CIRCULAR_SAW = "Circular Saw";
+        public const string WEAPON_CLAW_CUTTER = "Claw Cutter";
+        public const string WEAPON_FLAME_THROWER = "Flame Thrower";
+        public const string WEAPON_SLEDGE_HAMMER = "Sledge Hammer";
+        public const string WEAPON_SPINNNING_BLADE = "Spinning Blade";
+
+
         public int FuelLevel { get; set; } = 50;
         public int ConditionLevel { get; set; } = 50;
+        public string Weapon { get; set; }
+
+        public Vehicle()
+        {
+            Weapon = WEAPON_CIRCULAR_SAW;
+            Name = "Ash";
+        }
+
+        public Vehicle(string name, string weapon)
+        {
+            Weapon = weapon;
+            Name = name;
+        }
 
         public void HandleDamage(int damage)
         {
