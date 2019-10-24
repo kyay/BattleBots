3802184b5a8ed06881ccd7607e1d149cc428d4eb
Youssef Shoaib <yhshoaib@students.wcpss.net>
2019-10-24 07:30:41 -0400

    Add Player and Vehicle classes

---
+++ BattleBots/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleBots
+{
+    public class Player
+    {
+        private int score;
+        private int highScore;
+        private string name;
+
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                score = value;
+            }
+        }
+        public int HighScore
+        {
+            get
+            {
+                return highScore;
+            }
+            set
+            {
+                highScore = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Player()
+        {
+            Name = "Joe";
+        }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public void UpdateHighScore(int newScore)
+        {
+            HighScore = newScore > HighScore ? newScore : HighScore;
+        }
+
+        public void GainPoints(int points)
+        {
+            score += points;
+        }
+    }
+}
---
+++ BattleBots/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleBots
+{
+    public abstract class Vehicle
+    {
+        public int FuelLevel { get; set; } = 50;
+        public int ConditionLevel { get; set; } = 50;
+
+        public void HandleDamage(int damage)
+        {
+            ConditionLevel -= damage;
+        }
+
+        public void ConsumeFuel(int fuel)
+        {
+            FuelLevel -= fuel;
+        }
+
+        public abstract void Heal(int amount);
+        public abstract void Refuel(int amount);
+    }
+}
